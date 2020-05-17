9af49153e710337127a2af83c6fe252cd24dba62
Adesh Nalpet <adesh.nalpet@PP-C02Z66CALVCG.local>
2020-05-17 19:22:04 +0530

    Recusrion problem - Power set

--- src/Introduction/Recursion.java
+++ src/Introduction/Recursion.java
@@ -1,7 +1,9 @@
 package Introduction;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Applications :
@@ -40,6 +42,19 @@ public class Recursion {
         System.out.println("Max rope cuts: " + ropePieces(5, 1, 2, 5));
 
         powerSet("abc", "", 0);
+        hanoi(2, "A", "B", "C");
+
+        System.out.println(jos(5, 3));
+
+        System.out.println(atoi("12345", 4, 0));
+
+        List<Integer> ints = new ArrayList<Integer>();
+        ints.add(10);
+        ints.add(5);
+        ints.add(2);
+        ints.add(3);
+        ints.add(6);
+        subsetSum(ints, 0, 0);
 
     }
 
@@ -273,6 +288,118 @@ public class Recursion {
 
         powerSet(s, current, index + 1);
         powerSet(s, current + strings[index], index + 1);
+    }
+
+    /**
+     * TOWER OF HANOI
+     * For those who don't know this game,
+     * There are 3 towers A, B, C and tower A has certain number of disks
+     * And all the disks of tower A has to be moved to tower B (You must be thinking, "That's easy")
+     * But in the exact same order, so use tower B as the auxiliary tower.
+
+     * Rules :
+     * 1. Only one disk moves at a time.
+     * 2. No larger disc above smaller disc (Even in auxiliary tower).
+     * 3. Only the top disc of the tower can be moved.
+
+     * Output :
+     * Print the moves to be done to transfer all disc from A to C.
+     * Hint : A (3 disc), takes 7 steps to move to C (Note that you can use tower A as well as auxiliary tower).
+     *
+     * Another hint (n = 3) :
+     * 1. Move n - 1 discs from A to B, using C as auxiliary tower.
+     * 2. Move larger/ bottom disk from A to C
+     * 3. Move 2 discs in B to C using A as auxiliary tower
+     */
+    // FROM, AUX, TO
+    // We say C is aux for n == 1 as well, though aux is not used.
+
+    /**
+     * T(n) = T(n - 1) + T(n - 1) + 1
+     * T(n) = 2T(n - 1) + 1
+     * Therefore, in the recursion tree, every level : 1, 2, 4, 8..
+     * Complexity : 2^n
+     * Number of movements : 2^n - 1 (For n = 3, 7 movements).
+     */
+    public static void hanoi(int n, String a, String b, String c) {
+        if (n == 1) {
+            // Base case, when there is only one disc, move from A to C
+            System.out.println("Move disk " + n + " from " +  a + " to " + c);
+            return;
+        }
+        // Move n - 1 discs from A to B, using C as auxiliary tower.
+        hanoi(n - 1, a, c, b);
+
+        // Move n -1 discs in B to C using A as auxiliary tower
+        System.out.println("Move disk " + n + " from " +  a + " to " + c);
+        hanoi(n - 1, b, a, c);
+    }
+
+
+    /**
+     * JOSEPHUS PROBLEM
+     * n people standing in a circle, kill every kth person.
+     * And find the position of the survivor.
+     * n = 7  and k = 3
+     * Think of it as a Pistol is given to the 0th person and 2nd person is killed,
+     * Pistol is now with 3rd person (Including him, move 3) - 5th person is now killed.
+     * Pistol is now with 6th person - 1st person is killed.
+     * And so on. (Survivor is person with index 3 - Which is the output).
+     * Hint : When K = 2, the next person is killed (As the person holding the pistol is considered).
+     *
+     * Complexity :
+     * T(n) = T(n - 1) + c
+     * Therefore, O(n)
+     */
+    public static int jos(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+
+        return (jos(n - 1, k) + k) % n;
+    }
+
+    /**
+     * Find number of sub sets for the given sum
+     * Example : Input : {10, 5, 2, 3, 6}, sum = 8
+     * 2 subsets are possible with sum 8 : [5, 3] and [6, 2]
+     * Output : 2
+     * Corner case: If sum = 0, then output is always 1 (Empty set).
+
+     * This problem is very similar to "powerSet", where we print all the possible subsets of a string.
+     * Time complexity : 2^n
+     */
+    public static void subsetSum(List<Integer> ints, int current, int index) {
+
+        // Now that you've the SUM of all the possible subset, add a condition for the given sum.
+        if (index == ints.size()) {
+            System.out.println(current);
+            System.out.println("===========");
+            return;
+        }
+
+        subsetSum(ints, current, index + 1);
+        subsetSum(ints, current + ints.get(index), index + 1);
+
+    }
+
+    /* MORE PRACTICE PROBLEMS */
+    /* https://www.geeksforgeeks.org/recursion-practice-problems-solutions/ */
+
+    /**
+     * function takes a string (which represents an integer) as an argument and returns its value.
+     * The idea is separate the last digit, recursively compute result for remaining n-1 digits,
+     * multiply the result with 10 and add the obtained value to last digit.
+     */
+    public static int atoi(String str, int index, int sum) {
+        String[] strings = str.split("");
+
+        if (index == -1) {
+            return sum;
+        }
+
+        sum = sum + (Integer.parseInt(strings[index]) * (int) Math.pow(10, strings.length - 1 - index));
+        return atoi(str, index - 1, sum);
     }
 
 
