f32d46cb7ce764f7a8fcebe365f070ebe530253c
Adesh Nalpet <adesh.nalpet@PP-C02Z66CALVCG.local>
2020-06-02 23:28:45 +0530

    Fix pivot problem in BS

--- src/algorithms/search/BinarySearch.java
+++ src/algorithms/search/BinarySearch.java
@@ -146,6 +146,9 @@ public class BinarySearch {
      */
     public static int binarySearchPivot(int[] ints, int l, int h) {
         int mid = l + ((h - l)/2);
+        if ((mid == 0) || (mid == ints.length - 1)) {
+            return mid;
+        }
         if ((ints[mid] > ints[mid + 1]) && (ints[mid] > ints[mid - 1])) {
             return mid;
         }
@@ -205,11 +208,12 @@ public class BinarySearch {
      * Condition : Max(max1, max2) should be lesser than Min(min1, min2).
      * Once the condition is met : (Max(max1, max2) + Min(min1, min2))/2.
      * Time complexity : O(log n), where n is the size of the biggest array of the two.
+     * Assumption : n1 <=  n2
      */
     public static double median(int[] a1, int[] a2) {
 
-        int n1 = a1.length - 1;
-        int n2 = a2.length - 1;
+        int n1 = a1.length;
+        int n2 = a2.length;
 
         int start = 0;
         int end = n1;
@@ -218,11 +222,11 @@ public class BinarySearch {
             int i1 = (start + end)/2;
             int i2 = ((n1 + n2 + 1)/2) - i1;
 
-            int min1 = a1[i1];
-            int max1 = a1[i1 - 1];
+            int min1 = (i1 == n1) ? Integer.MAX_VALUE : a1[i1];
+            int max1 = (i1 == 0) ? Integer.MIN_VALUE : a1[i1 - 1];
 
-            int min2 = a2[i2];
-            int max2 = a2[i2 - 1];
+            int min2 = (i2 == n2) ? Integer.MAX_VALUE : a2[i2];
+            int max2 = (i2 == 0) ? Integer.MIN_VALUE : a2[i2 - 1];
 
             if (max1 <= min2 && max2 <= min1) {
                 if (((n1 + n2) % 2) == 0) {
