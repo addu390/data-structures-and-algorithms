636b36a3eab4ce33fd8dbfb35db3530386176c1f
Adesh Nalpet <390.adesh@gmail.com>
2020-11-02 15:46:19 +0530

    Graph data structure intro

--- src/dataStructures/Graph.java
+++ src/dataStructures/Graph.java
@@ -1,4 +1,24 @@
 package dataStructures;
 
+/**
+ * Think of the tree data structure as how friendship works,
+ * there can be mutual friends and hence cycles in a tree data structures.
+ * For such use cases where there is a possibility of circles in relations, we use graph data structure.
+ * There are two types:
+ * 1. Directed graphs (v1, v2) is v1 -> v2
+ * 2. Undirected graphs (v1, v2) == (v2, v1)
+ * Degree -> Number of edges going through it.
+ * In Directed: Incoming and outgoing degree.
+ *
+ * Terms:
+ * Walk (Path)
+ * Path (No repetitions of vertices) or Simple Path
+ * Cyclic: Begins and Ends with same vertex.
+ * Acyclic: When a graph has no cyclic path.
+ *
+ * Directed Acyclic Graphs (DAG)
+ *
+ * Weighted graphs: Graphs where edges are weighted (Can think of it as distance) - Example: Road networks.
+ */
 public class Graph {
 }
