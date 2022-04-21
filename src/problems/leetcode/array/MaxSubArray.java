6522636ff92fda32825e1237b7e46f874d1c1c66
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-04-21 20:59:44 -0300

    21 Apr 2022

---
+++ src/problems/leetcode/array/MaxSubArray.java
@@ -0,0 +1,14 @@
+package problems.leetcode.array;
+
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            result = Math.max(result, sum);
+        }
+        return result;
+    }
+}
