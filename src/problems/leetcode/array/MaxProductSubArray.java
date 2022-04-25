b75f363bd1d0315e2bbbb07207e7603e5b935bbb
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-04-25 17:15:53 -0300

    25 Apr 2022

---
+++ src/problems/leetcode/array/MaxProductSubArray.java
@@ -0,0 +1,25 @@
+package problems.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * Because of negative and positive number, keep track of max and min element,
+ * So that if num[i] is negative and minProduct is also negative -> choose nums[i] * minProduct.
+ * As compared to num[i] is positive and maxProduct is also positive -> choose nums[i] * maxProduct
+ */
+public class MaxProductSubArray {
+
+    public int maxProduct(int[] nums) {
+        int maxProduct = 1;
+        int minProduct = 1;
+        int result = Arrays.stream(nums).max().getAsInt();
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i] * maxProduct;
+            maxProduct = Math.max(nums[i] * minProduct, Math.max((nums[i] * maxProduct), nums[i]));
+            minProduct = Math.max(nums[i] * minProduct, Math.max(temp, nums[i]));
+            result = Math.max(result, maxProduct);
+        }
+        return result;
+    }
+}
