844407b775c1b9b3948026e3a38ab054ed39a7b7
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-04-27 15:45:38 -0300

    Add neetcode images

--- src/problems/leetcode/backTracking/CombinationSum.java
+++ src/problems/leetcode/backTracking/CombinationSum.java
@@ -3,16 +3,16 @@ package problems.leetcode.backTracking;
 import java.util.ArrayList;
 import java.util.List;
 
-// Refer to images/neetcode-39
+// Refer to images/neetcode-combinations-sum
 
 // Pointer - whether to choose the element or not, to ensure a unique set of combinations (same elements can however repeat).
 // Time complexity: 2^target
 public class CombinationSum {
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> currentCandidates = new ArrayList<>();
+        List<Integer> currentCombination = new ArrayList<>();
 
-        backtrack(candidates, result, currentCandidates, 0, 0, target);
+        backtrack(candidates, result, currentCombination, 0, 0, target);
 
         return result;
     }
@@ -20,14 +20,14 @@ public class CombinationSum {
     // i is the index pointer
     public static void backtrack(int[] candidates,
                                  List<List<Integer>> result,
-                                 List<Integer> currentCandidates,
+                                 List<Integer> currentCombination,
                                  int i,
                                  int currentTotal,
                                  int target) {
 
         if (currentTotal == target) {
             // To ensure that it's not the reference (Array copy at this stage).
-            result.add(new ArrayList<>(currentCandidates));
+            result.add(new ArrayList<>(currentCombination));
             return;
         }
 
@@ -36,12 +36,12 @@ public class CombinationSum {
         }
 
         // Consider ith candidate
-        currentCandidates.add(0, candidates[i]);
-        backtrack(candidates, result, currentCandidates, i, currentTotal + candidates[i], target);
+        currentCombination.add(0, candidates[i]);
+        backtrack(candidates, result, currentCombination, i, currentTotal + candidates[i], target);
 
         // Do not consider ith candidate
-        currentCandidates.remove(0);
-        backtrack(candidates, result, currentCandidates, i + 1, currentTotal, target);
+        currentCombination.remove(0);
+        backtrack(candidates, result, currentCombination, i + 1, currentTotal, target);
 
     }
 
