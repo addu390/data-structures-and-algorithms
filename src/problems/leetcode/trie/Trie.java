edc7ba12bd98e4b77cf2e7c18e4b259cb17f759a
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-04-28 19:10:50 -0300

    28 Apr 2022

---
+++ src/problems/leetcode/trie/Trie.java
@@ -0,0 +1,21 @@
+package problems.leetcode.trie;
+
+// Pronounced "Try", Prefix Tree - Commonly used for autocomplete and spellchecker.
+public class Trie {
+
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+        return true;
+    }
+
+    public boolean startsWith(String prefix) {
+        return true;
+    }
+}
