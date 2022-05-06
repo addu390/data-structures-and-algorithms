f73fba88b1218c4c4f0def0ab2b4f8b62185bbe7
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-05-05 23:35:29 -0300

    05 May 2022

---
+++ src/problems/leetcode/tree/LeafSimilarTrees.java
@@ -0,0 +1,31 @@
+package problems.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeafSimilarTrees {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> set1 = new ArrayList<>();
+        List<Integer> set2 = new ArrayList<>();
+
+        dfs(root1, set1);
+        dfs(root2, set2);
+
+        if (set1.equals(set2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void dfs(TreeNode node, List<Integer> set) {
+        if (node != null) {
+            if (node.left == null && node.right == null) {
+                set.add(node.val);
+            }
+
+            dfs(node.left, set);
+            dfs(node.right, set);
+        }
+    }
+}
