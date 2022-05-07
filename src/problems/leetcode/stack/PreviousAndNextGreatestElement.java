985ba18f03ffa6f737d98033b7b5c2b4d58c2924
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-05-07 13:53:44 -0300

    07 May 2022

---
+++ src/problems/leetcode/stack/PreviousAndNextGreatestElement.java
@@ -0,0 +1,43 @@
+package problems.leetcode.stack;
+
+import java.util.Stack;
+
+public class PreviousAndNextGreatestElement {
+
+    public static int[] previousGreatestElement(int[] array) {
+        int[] result = new int[array.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < array.length; i++) {
+            while (!stack.isEmpty() && stack.peek() <= array[i]) {
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(array[i]);
+        }
+
+        return result;
+    }
+
+    public static int[] nextGreatestElement(int[] array) {
+        int[] result = new int[array.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= array[i]) {
+                stack.pop();
+            }
+
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(array[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        previousGreatestElement(new int[]{13, 15, 12, 14, 16});
+        nextGreatestElement(new int[]{13, 15, 12, 14, 16});
+    }
+}
