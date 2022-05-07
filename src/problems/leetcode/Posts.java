705047dd5b6883aafe7a012e47c2c8c0634b3f19
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-05-07 17:56:21 -0300

    Top leetcode posts

---
+++ src/problems/leetcode/Posts.java
@@ -0,0 +1,48 @@
+package problems.leetcode;
+
+public class Posts {
+
+    /**
+     * Top LeetCode posts:
+     *
+     * 1) For Online Assessment + Phone Screen + Onsite Rounds:
+     * - https://leetcode.com/discuss/interview-question/398023/Microsoft-Online-Assessment-Questions
+     * - https://leetcode.com/discuss/interview-question/352460/Google-Online-Assessment-Questions
+     * - https://leetcode.com/discuss/interview-question/675445/Facebook-interview-experiences-All-Combined-from-LC-Till-Date-07-Jun-2020
+     * - https://leetcode.com/discuss/interview-question/488887/Amazon-Final-Interview-Questions-or-SDE1
+     * - https://leetcode.com/discuss/interview-question/790428/Facebook-Phone-Interview-Questions
+     * - https://leetcode.com/discuss/interview-question/460599/Blind-75-LeetCode-Questions
+     * - https://leetcode.com/discuss/interview-question/1590162/Amazon-Final-Interview-Questions-or-All-Combined-2021-or-SDE-and-New-Grad
+     *
+     * 2) For Behavioural Rounds:
+     * - https://leetcode.com/discuss/interview-question/437082/Amazon-Behavioral-questions-or-Leadership-Principles-or-LP
+     * - https://leetcode.com/discuss/interview-question/462901/behavioralleadership-questions-for-interviews
+     * - https://leetcode.com/discuss/interview-experience/1532708/tips-for-answering-few-tricky-behavioural-interview-questions
+     * - https://leetcode.com/discuss/interview-question/1729926/a-guide-for-behavioral-round
+     * - https://leetcode.com/discuss/interview-question/437631/Amazon-Leadership-Principle-(Behavioral)-Questions
+     *
+     * 3) For System Design Rounds (LLD + HLD):
+     * - https://leetcode.com/discuss/interview-question/1140451/Helpful-list-of-LeetCode-Posts-on-System-Design-at-Facebook-Google-Amazon-Uber-Microsoft
+     * - https://leetcode.com/discuss/interview-question/system-design/1205825/FANG-System-Design-Interview-Preparation-Master-Doc
+     * - https://leetcode.com/discuss/interview-question/system-design/691010/System-Design-for-Mobile-App-Developers
+     * - https://leetcode.com/discuss/general-discussion/1893215/repeatedly-asked-microsoft-onsite-questions-ds-lld-hld
+     * - https://leetcode.com/discuss/interview-question/1899352/Repeatedly-Asked-Meta-Onsite-Questions-or-HLD-%2B-DS
+     * - https://leetcode.com/discuss/interview-question/object-oriented-design/124738/5-flavors-of-singleton
+     * - https://leetcode.com/discuss/interview-question/object-oriented-design/125189/What-are-the-main-features-of-OOP
+     *
+     * 4) Top Interview Experiences to go through:
+     * - https://leetcode.com/discuss/interview-experience/765743/6%2B-months-of-hard-work-and-finally-offer-from-Facebook
+     * - https://leetcode.com/discuss/interview-experience/637356/Amazon-Apple-Facebook-or-L5-ICT4-E5-or-Seattle-or-April-2020-May-2020-Offer-Offer-Offer
+     * - https://leetcode.com/discuss/interview-experience/716202/AMZ-or-Google-or-Facebook-or-Offer-or-Rejector-Reject-or-My-journey-from-failure-to-offer-at-FAANG
+     * - https://leetcode.com/discuss/interview-experience/418395/Amazon-or-SDE1-or-Seattle-or-Oct-2019-Offer
+     * - https://leetcode.com/discuss/interview-experience/776280/Google-or-L3-or-Bangalore-or-July-2020-Offer
+     * - https://leetcode.com/discuss/interview-experience/455354/Give-Away-Mistakes-i-did-in-interview-and-which-should-be-avoided.
+     * - https://leetcode.com/discuss/interview-experience/646712/FAANG-Struggle-to-Offer-Amazon-or-SDE2-or-Seattle-or-May-2020-Offer
+     *
+     * 5) Some Strategies To Follow:
+     * - https://leetcode.com/discuss/study-guide/698684/Interview-Preparation-for-Beginners-DS-or-Algorithms-or-OS-or-System-Design
+     * - https://leetcode.com/discuss/study-guide/1177039/%22Practice-More-Learn-More%22-greater-Study-Guide-and-Interview-Preparation-Using-LEETCODE
+     * - https://leetcode.com/discuss/study-guide/623011/A-guide-for-dummies-(like-me)
+     * - https://leetcode.com/discuss/study-guide/2009997/How-to-get-started-with-DSA-and-practice-Leetcode-Efficiently
+     */
+}
