80f3f5999597baca21f211e3dbd4c81b3db4a346
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-05-20 10:29:55 -0300

    Use of Jackson

---
+++ src/systemDesign/rest/Json.java
@@ -0,0 +1,125 @@
+package systemDesign.rest;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Json {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Employee employee1 = objectMapper.readValue("{\"id\" : 1, \"firstName\": \"Mike\", \"lastName\": \"Botoskwi\", \"departmentId\": 2, \"salary\": 150000}", Employee.class);
+
+        Employee employee2 = new Employee(2, "Barret", "Jones", 3, 250000);
+        System.out.print(objectMapper.writeValueAsString(employee2));
+
+        // For List of JSON.
+        JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, Employee.class);
+        List<Employee> employeeList = objectMapper.readValue("[{\"id\" : 1, \"firstName\": \"Mike\", \"lastName\": \"Botoskwi\", \"departmentId\": 2, \"salary\": 150000}]",
+                                                          type);
+        List<Employee> employeeList2 = new ArrayList<>();
+        employeeList2.add(employee1);
+        employeeList2.add(employee2);
+
+        System.out.print(objectMapper.writeValueAsString(employeeList2));
+
+        // File
+        URL url = Json.class.getResource("input.json");
+        List<Employee> list = objectMapper.readValue(new File(url.toURI()), type);
+
+        Map<String, Employee> map = new HashMap<>();
+
+        for (Employee employee: list) {
+            String hash = String.format("%s_%s", employee.firstName, employee.lastName);
+            map.put(hash, employee);
+        }
+
+        for(Map.Entry<String, Employee> entry: map.entrySet()) {
+            System.out.print("\n");
+            System.out.print(entry.getKey());
+            System.out.print(entry.getValue());
+        }
+        for(String key: map.keySet()) {
+        }
+        for(Employee key: map.values()) {
+        }
+    }
+
+
+
+    static class Employee {
+        private Integer id;
+        private String firstName;
+        private String lastName;
+        private Integer departmentId;
+        private Integer salary;
+
+        public Employee() {
+
+        }
+
+        public Employee(String firstName, String lastName, Integer departmentId) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.departmentId = departmentId;
+        }
+
+        public Employee(Integer id, String firstName, String lastName, Integer departmentId, Integer salary) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.departmentId = departmentId;
+            this.salary = salary;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public Integer getDepartmentId() {
+            return departmentId;
+        }
+
+        public void setDepartmentId(Integer departmentId) {
+            this.departmentId = departmentId;
+        }
+
+        public Integer getSalary() {
+            return salary;
+        }
+
+        public void setSalary(Integer salary) {
+            this.salary = salary;
+        }
+    }
+}
